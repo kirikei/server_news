6080e36fea537530f88974e116238e71dc121621
kiritoshi <kiritoshi@db.soc.i.kyoto-u.ac.jp>
2015-06-22 16:41:14 +0900

    mediaの追加

--- app/api/api.rb
+++ app/api/api.rb
@@ -70,7 +70,7 @@ class API < Grape::API
 
       #top記事を持ってくる
       @top_links = Newsarticles.where(:pid => nil)
-      @top_links = @top_links.select('aid','image','summary','title','category','link','pubdate')
+      @top_links = @top_links.select('aid','image','summary','title','category','link','pubdate','media')
     end
 
     get :secret do
@@ -113,7 +113,7 @@ class API < Grape::API
       print "client_uuid #{client_uuid}\n"
 
       #historyへの登録
-      if read_aid != 0 then
+      if read_aid.length != 0 then
           calc_hist.register_history(client_uuid, read_aid, time)
       end
 
